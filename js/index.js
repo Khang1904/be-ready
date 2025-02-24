98a93d3415a06b8719119eb3db57398c13c9eb38
Khang1904 <tnkhang1109@gmail.com>
2025-02-24 20:26:45 +0700

    fix news loading

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const newsit = [document.getElementById('newsi0t'), document.getElementById('newsi1t'), document.getElementById('newsi2t')];
 const newsib = [document.getElementById('newsi0b'), document.getElementById('newsi1b'), document.getElementById('newsi2b')];
 
-const url = "https://newsapi.org/v2/everything?q=diseases&apiKey=7bc06aaa8e314c309652d4e0039e5c44"
+const url = "https://newsapi.org/v2/everything?q=diseases&pageSize=3&apiKey=7bc06aaa8e314c309652d4e0039e5c44"
 fetch(url)
   .then(response => response.json())
   .then(data => {
