69b0ada34c4c5a0b106c9d7754de5af2776cfec9
Khang1904 <tnkhang1109@gmail.com>
2025-04-17 19:42:31 +0700

    change api auth

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 const newsit = [document.getElementById('newsi0t'), document.getElementById('newsi1t'), document.getElementById('newsi2t')];
 const newsib = [document.getElementById('newsi0b'), document.getElementById('newsi1b'), document.getElementById('newsi2b')];
 
-const url = "https://newsapi.org/v2/everything?q=diseases&pageSize=3&apiKey=7bc06aaa8e314c309652d4e0039e5c44"
-fetch(url)
+const url = "https://newsapi.org/v2/everything?q=diseases&pageSize=3";
+fetch(url,{
+    method: 'GET',
+    headers: {
+      'X-Api-Key': '7bc06aaa8e314c309652d4e0039e5c44'
+    }
+})
   .then(response => response.json())
   .then(data => {
     for (let i = 0; i < 3; i++) {
