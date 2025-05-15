00c278d3db8cd62d6f673988bbbde215a22faef4
Khang1904 <tnkhang1109@gmail.com>
2025-05-15 20:52:43 +0700

    rework news and faq

--- js/news.js
+++ js/news.js
@@ -1,16 +1,11 @@
-const newsnt = [document.getElementById('newsn0t'), document.getElementById('newsn1t'), document.getElementById('newsn2t')];
-const newsnb = [document.getElementById('newsn0b'), document.getElementById('newsn1b'), document.getElementById('newsn2b')];
-const newscover = document.getElementById('newsni')
-
-const url = "https://gnews.io/api/v4/search?q=diseases&max=3&apikey=8de1bb755136930300a943b549cc1d1e";
+const url = "https://gnews.io/api/v4/search?q=diseases&apikey=8de1bb755136930300a943b549cc1d1e";
 fetch(url)
   .then(response => response.json())
   .then(data => {
-    for (let i = 0; i < 3; i++) {
-      newsnt[i].innerHTML = data.articles[i].title;
-      newsnb[i].innerHTML = data.articles[i].description;
+    for (let i = 0; i < 10; i++) {
+      document.getElementById(`newsnt${i}`).innerHTML = data.articles[i].title;
+      document.getElementById(`newsnb${i}`).innerHTML = data.articles[i].description;
+      document.getElementById(`newsna${i}`).href = data.articles[i].url;
     }
-    // newscover.src = data.articles[0].image;
-    // newscover.alt = data.articles[0].title;
   })
   .catch(error => console.log(error))
