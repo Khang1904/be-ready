eed2f1af72706e98974edc2719104907386c753f
Khang1904 <tnkhang1109@gmail.com>
2025-05-26 21:57:38 +0700

    fixing confusing nav-link and implement signout
    also, started getting info from db

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,5 +3,37 @@ const auth = firebase.auth();
 const db = firebase.firestore();
 
 auth.onAuthStateChanged((user) => {
-    console.log("User:", user.uid);
+    if (user) {
+        console.log("User is signed in:", user.uid);
+        db.collection('users').doc(user.uid).get()
+            .then((doc) => {
+                const userData = doc.data();
+                document.querySelector('#greet').innerHTML = `Welcome back, ${userData.username}`
+            })
+            .catch((error) => {
+                console.error("Error fetching user data:", error);
+            });
+    } else {
+        console.log("No user is signed in.");
+        window.location.href = './login.html';
+    }
+});
+
+function signOutUser() {
+    auth.signOut()
+        .then(() => {
+            console.log("User signed out.");
+            window.location.href = '../index.html';
+        })
+        .catch((error) => {
+            console.error("Sign out error:", error);
+        });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const signOutBtn = document.querySelector('.signout');
+    signOutBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        signOutUser();
+    });
 });
